a795ceffd62f8e014d97068c90c2f0749799035e
JHewitt <jaspathewit@gmail.com>
2021-12-22 17:36:30 +0100

    First checkin of question20

---
+++ dice.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Dice struct {
+	Top    int
+	North  int
+	East   int
+	South  int
+	West   int
+	Bottom int
+}
+
+func NewDice() Dice {
+	result := Dice{
+		Top:    6,
+		Bottom: 1,
+		North:  5,
+		East:   4,
+		South:  2,
+		West:   3,
+	}
+	return result
+}
+
+// String prints a string representation of a dice Top and 4 sides
+//  N
+// WTE
+//  S
+// ┌└┘┐│─
+
+func (d Dice) String() string {
+	var buffer bytes.Buffer
+	buffer.WriteString(fmt.Sprintf("┌──%d──┐\n", d.North))
+	buffer.WriteString(fmt.Sprintf("%d |%d| %d\n", d.West, d.Top, d.East))
+	buffer.WriteString(fmt.Sprintf("└──%d──┘\n", d.South))
+
+	return buffer.String()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module question20
+
+go 1.16
